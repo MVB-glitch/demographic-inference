da968ce133109804585861643de3c56de5903d93
郭嘉宏 <161590403+guousuides@users.noreply.github.com>
2026-05-12 12:12:46 +0900

    Delete src/demographic_inference directory

--- src/demographic_inference/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Demographic Inference Pipeline
-==============================
-Hybrid LLM + Dempster-Shafer Theory pipeline for inferring demographics
-(age, gender, location) from X (Twitter) user data.
-"""
-
-__version__ = "0.1.0"
--- src/demographic_inference/__main__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Allow running the package directly with: python -m demographic_inference"""
-
-from .cli import main
-
-if __name__ == "__main__":
-    main()
